b3482d38a07a7895544cd193155383b2c251772e
ehdtjs0612 <inko51366@naver.com>
2024-03-05 19:17:17 +0900

    feat: add sample login logic

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,30 @@
 import asyncWrap from '../utils/modules/async-wrap.module';
 import { validate } from '../middlewares/validate.middleware';
-import { param } from 'express-validator';
+import { body } from 'express-validator';
 import { Router } from 'express';
+import { IAccount } from '../interface/IAccount';
+import { authService } from '../services';
+import { ResponseEntity } from '../utils/modules/response-entity.module';
 
 const authRouter = Router();
 
-authRouter.get(
-  '/:accountIdx',
-  validate([param('accountIdx').notEmpty().isInt().withMessage('정수가 아닙니다').toInt()]),
-  asyncWrap(async (req, res, next) => {}),
+/**
+ * POST /auth/login
+ * @Body email
+ * @Body pw
+ * login & create session in redis
+ */
+authRouter.post(
+  '/login',
+  validate([body('loginId').notEmpty(), body('password').notEmpty()]),
+  asyncWrap(async (req, res, next) => {
+    const loginInput: IAccount.ILogin = req.body;
+
+    const userIdx = await authService.login(loginInput);
+    console.log(req.user.idx);
+
+    return res.send(ResponseEntity.SUCCESS_WITH(userIdx));
+  }),
 );
 
 export default authRouter;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,15 @@
 import { Service } from 'typedi';
 import { Prisma } from '../database/prisma.database';
+import { IAccount } from '../interface/IAccount';
 
 @Service()
 export class AuthService {
   constructor(private readonly prisma: Prisma) {}
+
+  async login({ loginId, password }: IAccount.ILogin) {
+    // 로그인 로직...
+
+    // 해당하는 사용자의 인덱스 반환
+    return 1;
+  }
 }
