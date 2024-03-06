be9e058b8c7dfe1dd40d6ef91c7f36755e36b9b9
ehdtjs0612 <inko51366@naver.com>
2024-03-06 19:52:14 +0900

    dev: learning.....

--- src/types/Express.d.ts
+++ src/types/Express.d.ts
@@ -1,9 +1,20 @@
-import { Session, SessionData } from 'express-session';
+import { CustomSessionData, Session, SessionData } from 'express-session';
 
 declare global {
   namespace Express {
     interface Request {
-      user: Session & Partial<SessionData>;
+      /**
+       * 1. req.user = req.session 을 위해 타입 정의
+       * 2. req.user의 타입을 일치시켜주기 위해 타입 정의
+       * req.user = {
+       *  cookie,
+       *  userId
+       * }
+       */
+      // user: (Session & Partial<CustomSessionData>) | SessionData;
+      // user: {
+      //   id: CustomSessionData['userId'];
+      // };
     }
   }
 }
