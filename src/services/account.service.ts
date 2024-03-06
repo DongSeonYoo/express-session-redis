772225cd3a48b4b9c802bb6d471165085954b5ce
ehdtjs0612 <inko51366@naver.com>
2024-03-06 19:51:02 +0900

    feat: add account service

---
+++ src/services/account.service.ts
@@ -0,0 +1,54 @@
+import { Service } from 'typedi';
+import { Prisma } from '../database/prisma.database';
+import { IAccount } from '../interface/IAccount';
+import { BadRequestException } from '../utils/modules/custom-error.module';
+
+@Service()
+export class AccountService {
+  constructor(private readonly prisma: Prisma) {}
+
+  async getAccountProfileDetail(userId: number): Promise<IAccount.IAccountProfileDetail> {
+    const result = await this.prisma.accountTb.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        name: true,
+        createdAt: true,
+      },
+    });
+
+    if (!result) {
+      throw new BadRequestException('해당하는 사용자가 존재하지 않습니다');
+    }
+
+    return {
+      id: result.id,
+      email: result.email,
+      password: result.password,
+      name: result.name,
+      createdAt: result.createdAt,
+    };
+  }
+
+  async getLoggedInUserInfo(userIds: number[]) /*: Promise<IAccount.ILoggedInUserList>*/ {
+    const result = await this.prisma.accountTb.findMany({
+      where: {
+        id: {
+          in: userIds,
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    return result;
+  }
+}
