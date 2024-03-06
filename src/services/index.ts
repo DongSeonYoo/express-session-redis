f0271f6bee0a83ec6bcc7ba52c53c82263a11541
ehdtjs0612 <inko51366@naver.com>
2024-03-06 16:40:23 +0900

    feat: add get AccountService Dependency

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 import Container from 'typedi';
 import { AuthService } from './auth.service';
+import { AccountService } from './account.service';
 
 export const authService = Container.get(AuthService);
+export const accountService = Container.get(AccountService);
