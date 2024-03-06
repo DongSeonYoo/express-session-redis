f65b657753c16771b27fd3612d45c4092128e9e3
ehdtjs0612 <inko51366@naver.com>
2024-03-06 19:40:17 +0900

    feat: add account interface

--- src/interface/IAccount.ts
+++ src/interface/IAccount.ts
@@ -8,4 +8,10 @@ export interface IAccount {
 
 export namespace IAccount {
   export interface ILogin extends Pick<IAccount, 'email' | 'password'> {}
+
+  export interface ISignup extends Pick<IAccount, 'email' | 'password' | 'name'> {}
+
+  export interface IAccountProfileDetail extends IAccount {}
+
+  export interface ILoggedInUserList extends Pick<IAccount, 'id' | 'name' | 'createdAt'> {}
 }
