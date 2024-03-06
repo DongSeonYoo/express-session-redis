e9f0027e7c3df6db1d853a89bf001f3da02cca82
ehdtjs0612 <inko51366@naver.com>
2024-03-06 19:49:34 +0900

    feat: check req.session in auth-guard middleware

--- src/middlewares/auth-guard.ts
+++ src/middlewares/auth-guard.ts
@@ -1,8 +1,13 @@
 import { RequestHandler } from 'express';
 import asyncWrap from '../utils/modules/async-wrap.module';
+import { UnauthorizedException } from '../utils/modules/custom-error.module';
 
 export const loginAuthGuard = (): RequestHandler => {
   return asyncWrap(async (req, res, next) => {
+    if (!req.session || !req.session.userId) {
+      throw new UnauthorizedException('로그인 후 이용가능합니다');
+    }
+
     return next();
   });
 };
