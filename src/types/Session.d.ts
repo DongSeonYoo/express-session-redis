ceed2c4cf22c25d22f3550422e5dc4ef1b713d73
ehdtjs0612 <inko51366@naver.com>
2024-03-07 16:49:06 +0900

    feat: add loggedInAt property in CustomSessionData

--- src/types/Session.d.ts
+++ src/types/Session.d.ts
@@ -3,6 +3,7 @@ import { IAccount } from '../interface/IAccount';
 declare module 'express-session' {
   export interface CustomSessionData {
     userId: IAccount['id'];
+    loggedInAt: Date;
   }
 
   interface SessionData extends CustomSessionData {}
