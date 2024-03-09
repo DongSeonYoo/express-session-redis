21439ceccb23fd201cc0b1c3286c6c3940983690
ehdtjs0612 <inko51366@naver.com>
2024-03-09 21:02:50 +0900

    feat: add redisService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,8 @@
 import Container from 'typedi';
 import { AuthService } from './auth.service';
 import { AccountService } from './account.service';
+import { RedisService } from '../database/redis.database';
 
 export const authService = Container.get(AuthService);
 export const accountService = Container.get(AccountService);
+export const redisService = Container.get(RedisService);
