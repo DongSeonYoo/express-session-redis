d7092227e62fd0eca3e18070f18b3991d1e67dd0
ehdtjs0612 <inko51366@naver.com>
2024-03-09 21:01:38 +0900

    feat: add role property to CustomSessionData

--- src/types/Session.d.ts
+++ src/types/Session.d.ts
@@ -1,8 +1,10 @@
 import { IAccount } from '../interface/IAccount';
+import { Role } from '../interface/IRole';
 
 declare module 'express-session' {
   export interface CustomSessionData {
     userId: IAccount['id'];
+    role: Role;
     loggedInAt: Date;
   }
 
